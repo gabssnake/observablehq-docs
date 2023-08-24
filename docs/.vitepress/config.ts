d593bc925724193b1545252ace9a9a026fbe5942
Josh Earlenbaugh <joshearlenbaugh@gmail.com>
2023-08-24 12:15:47 -0400

    Structure1 (#4)
    * Updated structure.
    
    * New structure collapsed at top level.
    
    * Modified index some.
    
    * removed folder that should be ignored.
    
    * Added .gitignore
    
    * Fixed comma
    
    ---------
    
    Co-authored-by: Josh Earlenbaugh <josh@observablehq.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -21,14 +21,13 @@ export default defineConfig({
     ["script", {}, "window.dataLayer=window.dataLayer||[];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js',new Date());\ngtag('config','G-9B88TP6PKQ');"]
   ],
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     logo: {
       light: "/observable-light.svg",
       dark: "/observable-dark.svg"
     },
     nav: [
-      {text: "Getting Started", link: "/"},
-      {text: "Examples", link: "/markdown-examples"}
+      {text: "Sign up", link: "/"},
+      {text: "Quickguides", link: "/"}
     ],
     sidebar: [
       {
@@ -36,60 +35,269 @@ export default defineConfig({
         items: [
           {text: "Why Observable", link: "/"},
           {text: "A taste of Observable", link: "/"},
-          {text: "Creating your account", link: "/"},
-          {text: "Learn Observable"}
+          {text: "Learning Observable", link: "/"},
+          {text: "Enterprise and Pro team onboarding", link: "/"},
+          {text: "Interactive tutorials", link: "/"},
+          {text: "Quickguides", link: "/"}
         ]
       },
       {
-        text: "Notebooks",
+        text: "Cells",
+        collapsed: true,
         items: [
-          {text: "Cell modes", link: "/"},
-          {text: "Data Table cell", link: "/"},
-          {text: "Chart cell", link: "/"},
-          {text: "Add Data", link: "/"},
-          {text: "Importing", link: "/"},
-          {text: "Debugging", link: "/"},
-          {text: "Tinker Mode", link: "/"},
-          {text: "Fork, Suggest, Merge", link: "/"},
-          {text: "Saving & History", link: "/"},
-          {text: "Collaboration", link: "/"}
-        ]
-      },
-      {
-        text: "Collections",
-        items: [
-          {text: "Placeholder"}
-        ]
-      },
-      {
-        text: "Dashboards",
-        items: [
-          {text: "Placeholder"}
-        ]
-      },
-      {
-        text: "Notifications",
-        items: [
-          {text: "Placeholder"}
-        ]
-      },
-      {
-        text: "Accounts & Workspaces",
-        items: [
-          {text: "Security", link: "/"},
-          {text: "Transfer", link: "/"},
-          {text: "Membership", link: "/"},
-          {text: "Databases", link: "/"},
-          {text: "Permissions", link: "/"},
-          {text: "Secrets", link: "/"},
-          {text: "API / Notebook Keys", link: "/"},
-          {text: "Suggestions", link: "/"},
-          {text: "Profile", link: "/"},
-          {text: "Trash", link: "/"}
-        ]
-      }
+          {text: "Cell Basics", link: "/"},
+          {text: "Cell Modes",
+          collapsed: true,
+           items: [
+             {text: "JavaScript",
+              items: [
+                {text: "Introduction to Code"},
+                {text: "Observable JavaScript"},
+                {text: "Require in Observable", link: "/"}
+              ]
+             },
+             {text: "Text", link: "/"},
+             {text: "Data Table", link: "/"},
+             {text: "Chart", link: "/"},
+             {text: "HTML", link: "/"},
+             {text: "SQL", link: "/"},
+             {text: "Mathematical", link: "/"}
+           ]
+          }
+         ]
+        },
+        {
+            text: "Data",
+            collapsed: true,
+           items: [
+            { 
+              text: "Files",
+              collapsed: true,
+              items: [
+                {text: "File attachments", link: "/"},
+                {text: "Supported file types",
+                collapsed: true,
+                 items: [
+                  {text: "XLSX", link: "/"},
+                  {text: "JSON", link: "/"},
+                  {text: "CSV", link: "/"},
+                  {text: "ZIP", link: "/"},
+                  {text: "SQLite", link: "/"},
+                  {text: "DuckDB", link: "/"},
+                  {text: "Apache Arrow", link: "/"},
+                  {text: "Apache Parquet", link: "/"}
+                 ]
+                },
+                {text: "Cloud files", link: "/"},
+                {text: "Supported integrations",
+                collapsed: true,
+                 items: [
+                  {text: "Microsoft Teams", link: "/"},
+                  {text: "Google Drive", link: "/"}
 
-    ],
+                 ]
+                }
+              ]
+
+            },
+            {
+              text: "Databases",
+              collapsed: true,
+              items: [
+                {text: "Connecting to databases", link: "/"},
+                {text: "Database panel and schema viewer", link: "/"},
+                {text: "Self-hosted proxies", link: "/"},
+                {text: "Database clients",
+                collapsed: true,
+                 items: [
+                  {text: "Amazon Red Shift", link: "/"},
+                  {text: "BigQuery", link: "/"},
+                  {text: "Databricks", link: "/"},
+                  {text: "Mongo SQL", link: "/"},
+                  {text: "MySQL", link: "/"},
+                  {text: "Oracle", link: "/"},
+                  {text: "Postgress", link: "/"},
+                  {text: "Snowflake", link: "/"},
+                  {text: "SQL Server", link: "/"},
+                  {text: "SQLite", link: "/"},
+                  {text: "DuckDB", link: "/"},
+                  {text: "HEAVY.AI", link: "/"},
+                  {text: "QuestDB", link: "/"},
+                  {text: "Datasette", link: "/"},
+                  {text: "Seafowl", link: "/"},
+                  {text: "Dolt", link: "/"}
+                 ]}
+              ]
+            },
+            {
+              text: "APIs",
+              collapsed: true,
+              items: [
+                {text: "AWS S3", link: "/"},
+                {text: "Figma", link: "/"},
+                {text: "GitHub", link: "/"},
+                {text: "Stripe", link: "/"},
+                {text: "Bitbucket", link: "/"}
+              ]
+            },
+            {
+              text: "Datasets",
+              collapsed: true,
+              items: [
+                {text: "Sample datasets", link: "/"},
+                {text: "Curated datasets", link: "/"}
+              ]
+            }
+           
+           ],
+          },
+          {
+            text: "AI Assist",
+            collapsed: true,
+            items: [
+              {text: "Developing with AI Assist", link: "/"}
+            ]
+          },
+          {
+            text: "Inputs",
+            collapsed: true,
+            items: [
+              {text: "Using inputs in Observable", link: "/"},
+              {text: "Input types",
+              collapsed: true,
+               items: [
+                 {text: "Button", link: "/"},
+                 {text: "Toggle", link: "/"},
+                 {text: "Checkbox", link: "/"},
+                 {text: "Radio", link: "/"},
+                 {text: "Range", link: "/"},
+                 {text: "Select", link: "/"},
+                 {text: "Text", link: "/"},
+                 {text: "Textarea", link: "/"},
+                 {text: "Date/Datetime", link: "/"},
+                 {text: "Color", link: "/"},
+                 {text: "Search", link: "/"},
+                 {text: "Table", link: "/"},
+                 {text: "File", link: "/"},
+                 {text: "Form", link: "/"}
+               ]
+              }
+            ]
+          },
+          {
+            text: "Reusing Work",
+            collapsed: true,
+            items: [
+              {text: "Forking", link: "/"},
+              {text: "Importing",
+              collapsed: true,
+               items: [
+                {text: "Imports", link: "/"},
+                {text: "Version locking for imports", link: "/"},
+               ]
+              },
+              {text: "Templates",
+              collapsed: true,
+               items: [
+                {text: "Using templates", link: "/"},
+                {text: "Custom templates", link: "/"}
+               ]
+              }
+            ]
+          },
+          {
+            text: "Embedding",
+            collapsed: true,
+            items: [
+              {text: "Basic Embedding", link: "/"},
+              {text: "Advanced Embedding and downloading", link: "/"},
+              {text: "Embed a notebook in a React app", link: "/"},
+              {text: "Auto-resizing embed", link: "/"},
+              {text: "API and notebook keys", link: "/"},
+              {text: "Troubleshooting embedding", link: "/"}
+
+            ]
+          },
+          {
+            text: "Saving and History",
+            collapsed: true,
+            items: [
+              {text: "How saving works", link: "/"},
+              {text: "Notebook history", link: "/"}
+            ]
+          },
+          {
+            text: "Collaborating",
+            collapsed: true,
+            items: [
+              {text: "Sharing work",
+              collapsed: true,
+               items: [
+                {text: "Sharing", link: "/"},
+                {text: "Searching", link: "/"},
+                {text: "Pausing live edits in public notebooks", link: "/"},
+                {text: "Tags: Organizing work"},
+                {text: "Licenses"}
+               ]
+              },
+              {text: "Comments", link: "/"},
+              {text: "Suggestions", link: "/"},
+              {text: "Following users and teams", link: "/"},
+              {text: "Tinker mode: temporary edits to a notebook", link: "/"}
+            ]
+          },
+          {
+            text: "Debugging",
+            collapsed: true,
+            items: [
+              {text: "Safe mode", link: "/"},
+              {text: "Minimap", link: "/"},
+              {text: "Module require debugger", link: "/"},
+              {text: "How to require stubborn modules", link: "/"}
+            ]
+          },
+          {
+            text: "Accounts and Workspaces",
+            collapsed: true,
+            items: [
+              {text: "Creating your account", link: "/"},
+              {text: "Deleting your account", link: "/"},
+              {text: "Workspace settings", link: "/"},
+              {text: "Advantages of team workspaces", link: "/"},
+              {text: "Team creation and administration", link: "/"},
+              {text: "Using transfer to move notebooks", link: "/"},
+              {text: "Custom SSO for Teams", link: "/"},
+              {text: "Workspace Audit Logs", link: "/"},
+              {text: "Microsoft Teams integration", link: "/"}
+            ]
+          },
+          {
+            text: "Security",
+            collapsed: true,
+            items: [
+              {text: "Observable's Security model", link: "/"},
+              {text: "Security and data access in Observable", link: "/"},
+              {text: "Using secrets with Observable", link: "/"}
+            ]
+          },
+          {
+            text: "Misc",
+            collapsed: true,
+            items: [
+              {text: "Observable terminology", link: "/"},
+              {text: "Observable FAQ", link: "/"},
+              {text: "Standard library", link: "/"},
+              {text: "Don't do this!", link: "/"},
+              {text: "Keyboard shortcuts", link: "/"},
+              {text: "Find and replace", link: "/"}, 
+              {text: "Awaiting visibility", link: "/"},
+              {text: "Custom notebook URLs", link: "/"},
+              {text: "Mixing queries and arrays into SQL", link: "/"}
+
+            ]
+          }
+        ],
+    
     search: {
       provider: "local"
     },
--- docs/index.md
+++ docs/index.md
@@ -5,7 +5,7 @@ layout: home
 hero:
   name: "Observable Docs"
   text: "[DRAFT] How to work in Observable"
-  tagline: Your spot for all questions Observable
+  tagline: Your spot for all questions Observable.
   actions:
     - theme: brand
       text: Markdown Examples
@@ -15,17 +15,17 @@ hero:
       link: /api-examples
 
 features:
-  - title: Fundamentals
-    details: The basics to getting up and running in Observable
+  - title: Learning Observable
+    details: The basics to getting up and running in Observable.
     link: /fundamentals/getting-started
   - title: Accounts & Workspaces
-    details: Set up your Observable account and configure your workspace
+    details: Set up your Observable account and configure your workspace.
   - title: Data
     details: Securely get data in and out of Observable
   - title: Sharing & Collaboration
-    details: Get your work infront of others, in notebooks or anywhere on the web
+    details: Get your work infront of others, in notebooks or anywhere on the web.
   - title: Inputs
-    details: Obserbable Inputs allows you to quickly add interactivity to your analysis
+    details: Observable Inputs allows you to quickly add interactivity to your analysis
   - title: Observable JavaScript
     details: Learn about Observable JavaScript makes development a breeze
   - title: Debugging
